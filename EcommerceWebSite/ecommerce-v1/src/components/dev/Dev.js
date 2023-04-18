9aeabf3f73401c02de50eb8c3d890f68b3ea87d4
PrerakPatelCS <62869089+PrerakPatelCS@users.noreply.github.com>
2023-04-18 03:18:42 -0400

    Added Dev tool

--- EcommerceWebSite/ecommerce-v1/src/components/dev/Dev.js
+++ EcommerceWebSite/ecommerce-v1/src/components/dev/Dev.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Dev = () => {
+function Dev () {
+  const [isReady, setIsReady] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    price: '',
+    imageUrl: '',
+    category: ''
+  });
+
+  function handleInputChange(event) {
+    // check if all required fields have a value
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+    const name = document.getElementById('name').value;
+    const description = document.getElementById('description').value;
+    const price = document.getElementById('price').value;
+    const imageUrl = document.getElementById('imageUrl').value;
+    const category = document.getElementById('category').value;
+    if (name && description && price && imageUrl && category) {
+      setIsReady(true);
+    } else {
+      setIsReady(false);
+    }
+    
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const jsonFormData = JSON.stringify(formData);
+    console.log(jsonFormData);
+    console.log(formData);
+    setFormData({
+      name: '',
+      description: '',
+      price: '',
+      imageUrl: '',
+      category: ''
+    });
+    setIsReady(false);
+    // handle form submission
+  }
+
+
   return (
-    <div>Dev</div>
+    <div className='add-apparel-form'>
+      <div className='container'>
+        <form className='login-form' onSubmit={handleSubmit}>
+          <label form="name">Name</label>
+          <input type='text' placeholder="Name" id='name' name='name' value={formData.name} onChange={handleInputChange} />
+          <label form="description">Description</label>
+          <input type='text' placeholder="Description" id='description' name='description' value={formData.description} onChange={handleInputChange} />
+          <label form="price">Price</label>
+          <input type='number' placeholder="Price" step='0.01' min='0' id='price' name='price' value={formData.price} onChange={handleInputChange} />
+          <label form="imageUrl">ImageUrl</label>
+          <input type='text' placeholder="ImageUrl" id='imageUrl' name='imageUrl' value={formData.imageUrl} onChange={handleInputChange} />
+          <label form="category">Category</label>
+          <input type='text' placeholder="Category" id='category' name='category' value={formData.category} onChange={handleInputChange} />
+          <button type='submit' disabled={!isReady}>Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
